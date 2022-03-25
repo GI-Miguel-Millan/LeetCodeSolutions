c1d4c021f600d22595f758a8481ab04a135276c6
Miguel M <miggy221@gmail.com>
2022-03-25 09:04:48 -0400

    Add project files.

---
+++ LeetCodeSolutions.cpp
@@ -0,0 +1,12 @@
+﻿// LeetCodeSolutions.cpp : Defines the entry point for the application.
+//
+
+#include "LeetCodeSolutions.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
