5c53a69603fb9dfb529590558db45b2204fcdba0
Miguel M <miggy221@gmail.com>
2022-03-27 13:05:22 -0400

    longest-common-prefix skip first string

--- 0011-Longest-Common-Prefix/0011-Longest-Common-Prefix.cpp
+++ 0011-Longest-Common-Prefix/0011-Longest-Common-Prefix.cpp
@@ -12,7 +12,7 @@ std::string longestCommonPrefix(std::vector<std::string>& strs)
 	std::string prefix = "";
 	for (int i = 0; i < strs[0].size(); i++)
 	{
-		for (int j = 0; j < strs.size(); j++)
+		for (int j = 1; j < strs.size(); j++)
 		{
 			if (i == strs[j].size() || strs[j][i] != strs[0][i])
 				return prefix;
