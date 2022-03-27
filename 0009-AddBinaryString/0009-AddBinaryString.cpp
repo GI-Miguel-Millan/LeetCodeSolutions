b99d8f34700c51d825a0653ac028f33f74e20e69
Miguel M <miggy221@gmail.com>
2022-03-27 11:17:34 -0400

    Binary string addition second attempt (slow...)

--- 0009-AddBinaryString/0009-AddBinaryString.cpp
+++ 0009-AddBinaryString/0009-AddBinaryString.cpp
@@ -7,54 +7,61 @@
 #include <limits>
 #include <iostream>
 
-int parseBinary(std::string str)
-{
-	int decimalValue = 0;
-	int power = 0;
-	for (int i = str.size() - 1; i >= 0; i--)
-	{
-		if (str[i] == '1')
-			decimalValue += pow(2, power);
-		power++;
-	}
-	return decimalValue;
-}
-
-std::string toBinaryString(int decimalValue)
-{
-	if (decimalValue == 0) return "0";
-	std::string str;
-
-	int numBits = std::numeric_limits<int>::digits;
-	for (int i = 0; i < numBits; i++)
-	{
-		if (decimalValue & 1)
-			str += "1";
-		else
-			str += "0";
-
-		decimalValue = decimalValue >> 1;
-
-		if (decimalValue == 0)
-			break;
-	}
-
-	int halfPoint = str.size() / 2;
-	for (int i = 0, j = str.size() - 1; i < halfPoint; i++, j--)
-		std::swap(str[i], str[j]);
-
-	return str;
-}
-
 std::string addBinary(std::string a, std::string b)
 {
-	int aVal = parseBinary(a);
-	int bVal = parseBinary(b);
-	return toBinaryString(aVal + bVal);
+	std::string longerString = a;
+	std::string shorterString = b;
+	std::string result;
+
+	if (a.size() < b.size())
+	{
+		longerString = b;
+		shorterString = a;
+	}
+
+	int carry = 0;
+	int longerStringIndex = longerString.size() - 1;
+	for (int i = shorterString.size() - 1; i >= 0; i--, longerStringIndex--)
+	{
+		int lVal = longerString[longerStringIndex] == '1' ? 1 : 0;
+		int sVal = shorterString[i] == '1' ? 1 : 0;
+		int digitSum = lVal + sVal + carry;
+
+		result += (digitSum % 2 == 0) ? '0' : '1';
+		carry = digitSum >> 1;
+	}
+
+	while (longerStringIndex >= -1)
+	{
+		if (longerStringIndex < 0)
+		{
+			if (carry == 1)
+				result += "1";
+			break;
+		}
+
+		int lVal = longerString[longerStringIndex] == '1' ? 1 : 0;
+		int digitSum = lVal + carry;
+
+		result += (digitSum % 2 == 0) ? '0' : '1';
+		carry = digitSum >> 1;
+
+		longerStringIndex--;
+	}
+
+	int halfPoint = result.size() / 2;
+	for (int i = 0, j = result.size() - 1; i < halfPoint; i++, j--)
+		std::swap(result[i], result[j]);
+
+	return result;
 }
 
 int main()
 {
 	std::cout << "1001 + 1111 = " << addBinary("1001", "1111") << std::endl;
+	std::cout << "1 + 11 = " << addBinary("1", "11") << std::endl;
+	std::cout << "1010 + 1011 = " << addBinary("1010", "1011") << std::endl;
+	std::cout << "1 + 111 = " << addBinary("1", "111") << std::endl;
+	std::cout << "100 + 110010 = " << addBinary("100", "110010") << std::endl;
 	return 0;
 }
