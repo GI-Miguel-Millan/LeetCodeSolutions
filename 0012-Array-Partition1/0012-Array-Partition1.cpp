0ae7209d384d6a90ddc6602dd89ccafa8966e497
Miguel M <miggy221@gmail.com>
2022-03-27 21:36:52 -0400

    ArrayPairSum (Array Partition 1)

---
+++ 0012-Array-Partition1/0012-Array-Partition1.cpp
@@ -0,0 +1,23 @@
+﻿// 0012-Array-Partition1.cpp : Source file for your target.
+//
+
+#include "0012-Array-Partition1.h"
+#include <algorithm>
+#include <vector>
+
+int arrayPairSum(std::vector<int>& nums)
+{
+	std::sort(nums.begin(), nums.end());
+
+	int sum = 0;
+	for (int i = 0; i < nums.size(); i += 2)
+		sum += nums[i];
+
+	return sum;
+}
+
+int main()
+{
+	
+	return 0;
+}
